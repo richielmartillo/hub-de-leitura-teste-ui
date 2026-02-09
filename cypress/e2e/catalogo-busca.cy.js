09dd9390318cec16d906f01355afe27a490df6f9
Richard Marlon Balestrim <richardbalestrim8@gemail.com>
2026-02-08 21:27:14 -0300

    Alterações do catalogo-busca.cy.js

--- cypress/e2e/catalogo-busca.cy.js
+++ cypress/e2e/catalogo-busca.cy.js
@@ -1,21 +1,34 @@
 /// <reference types="cypress"/>
-import catalogo from "../fixtures/livros.json"
+import catalogo from '../fixtures/livros.json'
 describe('Funcionalidade: Busca no catálogo', () => {
-    
-    beforeEach(() => {
-        cy.visit('catalog.html')
-    });
+  beforeEach(() => {
+    cy.visit('catalog.html')
+  })
 
-    it('Deve fazer a busca do livro 1984 com sucesso', () => {
-    
-        cy.get('#search-input').type('1984')
-        cy.get('.card-title > .text-dark').should('contain', '1984')
-    });
+  it('Deve fazer a busca do livro 1984 com sucesso', () => {
+    cy.get('#search-input').type('1984')
+    cy.get('.card-title > .text-dark').should('contain', '1984')
+  })
 
-     it('Deve fazer a busca de um livro do arquivo da massa de dados', () => {
-    
-        cy.get('#search-input').type(catalogo[2].livro)
-        cy.get('.card-title > .text-dark').should('contain', catalogo[2].livro)
-    
-    });
-});
+  it('Deve fazer a busca de um livro do arquivo da massa de dados', () => {
+    cy.get('#search-input').type(catalogo[2].livro)
+    cy.get('.card-title > .text-dark').should('contain', catalogo[2].livro)
+  })
+
+  it('Deve fazer abusca de um livro usando Fixture', () => {
+    cy.fixture('livros').then((cat) => {
+      cy.get('#search-input').type(cat[2].livro)
+      cy.get('.card-title > .text-dark').should('contain', cat[2].livro)
+    })
+  })
+
+  it.only('Deve validar todos os livros da lista', () => {
+    cy.fixture('livros').then((cat) =>{
+        cat.forEach(item =>{
+            cy.get('#search-input').clear().type(item.livro)
+            cy.get('.card-title > .text-dark').should('contain', item.livro)
+
+        })
+    })
+  });
+})
