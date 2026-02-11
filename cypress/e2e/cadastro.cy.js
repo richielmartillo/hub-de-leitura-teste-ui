d67f82cd05385b5c44808ff84fb5b5f210aabed5
Richard Marlon Balestrim <richardbalestrim8@gemail.com>
2026-02-11 12:14:18 -0300

    automação do catalogo e login

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -13,35 +13,13 @@ describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
      cy.screenshot()
   });
 
-  it('Deve fazer cadastro com sucesso, usando função JS', () => {
-    let email = `teste${Date.now()}@teste.com`
-    cy.get('#name').type('Richard Marlon Balestrim')
-    cy.get('#email').type(email)
-    cy.get('#phone').type('44999430660')
-    cy.get('#password').type('123,5Aire')
-    cy.get('#confirm-password').type('123,5Aire')
-    cy.get('#terms-agreement').check()
-    cy.get('#register-btn').click()
-    cy.url().should('include', 'dashboard')
-  })
-
-  it('Deve fazer cadastro com sucesso usando Faker', () => {
-    let nome = faker.person.fullName()
-    let email = faker.internet.email()
-
-    cy.get('#name').type(nome)
-    cy.get('#email').type(email)
-    cy.get('#phone').type('44999430660')
-    cy.get('#password').type('richar.@m8')
-    cy.get('#confirm-password').type('richar.@m8')
-    cy.get('#terms-agreement').check()
-    cy.get('#register-btn').click()
-
-    cy.url().should('include', 'dashboard')
-    cy.get('#user-name').should('contain', nome)
-  })
   
 
+
+
+
+
+  
   it('Deve bloquear cadastro quando campos obrigatorios estão vazios', () => {
     cy.get('#register-btn').click()
     // exemplos (ajuste conforme seu HTML)
@@ -85,6 +63,35 @@ describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
     cy.url().should('include', 'register')
    })
 
+
+   it('Deve fazer cadastro com sucesso, usando função JS', () => {
+    let email = `teste${Date.now()}@teste.com`
+    cy.get('#name').type('Richard Marlon Balestrim')
+    cy.get('#email').type(email)
+    cy.get('#phone').type('44999430660')
+    cy.get('#password').type('123,5Aire')
+    cy.get('#confirm-password').type('123,5Aire')
+    cy.get('#terms-agreement').check()
+    cy.get('#register-btn').click()
+    cy.url().should('include', 'dashboard')
+  })
+
+  it('Deve fazer cadastro com sucesso usando Faker', () => {
+    let nome = faker.person.fullName()
+    let email = faker.internet.email()
+    cy.get('#name').type(nome)
+    cy.get('#email').type(email)
+    cy.get('#phone').type('44999430660')
+    cy.get('#password').type('richar.@m8')
+    cy.get('#confirm-password').type('richar.@m8')
+    cy.get('#terms-agreement').check()
+    cy.get('#register-btn').click()
+    cy.url().should('include', 'dashboard')
+    cy.get('#user-name').should('contain', nome)
+  })
+  
+
+
     it('Deve preencher cadastro com sucesso - Usando comando customisado', () => {
     let email = `teste${Date.now()}@teste.com`            
     let nome = faker.person.fullName({sex: 'male'})
@@ -101,7 +108,13 @@ describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
     cadastroPage.preencherCadastro('Richard Marlon Balestrim', email, '44999430660', 'richar.@8m', 'richar@8m')
   });
 
- it('Deve validar mensagem ao tentar cadastrar sem prencher nome', () => {
+ 
+ 
+ 
+ 
+ 
+ 
+  it('Deve validar mensagem ao tentar cadastrar sem prencher nome', () => {
   
   cadastroPage.preencherCadastro('', 'richardbalestrim8@gmail.com', '44999430660', 'richar@8m', 'richr@8m')
   cy.get(':nth-child(1) > .invalid-feedback').should('contain', 'Nome deve ter pelo menos 2 caracteres')
